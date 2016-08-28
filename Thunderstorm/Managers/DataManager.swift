021e3db97c90f83ea32ce06d39bdeab5489fcdee
Bart Jacobs <developer@bartjacobs.com>
2016-08-28 15:06:45 +0200

    Add ability to DataManager to fetch and process weather data

--- Thunderstorm/Managers/DataManager.swift
+++ Thunderstorm/Managers/DataManager.swift
@@ -8,6 +8,62 @@
 
 import Foundation
 
+enum DataManagerError: Error {
+
+    case Unknown
+    case FailedRequest
+    case InvalidResponse
+    
+}
+
 class DataManager {
+
+    typealias WeatherDataCompletion = (AnyObject?, DataManagerError?) -> ()
+
+    let baseURL: URL
+
+    // MARK: - Initialization
+
+    init(baseURL: URL) {
+        self.baseURL = baseURL
+    }
+
+    // MARK: - Requesting Data
+
+    func weatherDataForLocation(latitude: Double, longitude: Double, completion: WeatherDataCompletion) {
+        // Create URL
+        let URL = baseURL.appendingPathComponent("\(latitude),\(longitude)")
+
+        // Create Data Task
+        URLSession.shared.dataTask(with: URL) { (data, response, error) in
+            self.didFetchWeatherData(data: data, response: response, error: error, completion: completion)
+            }.resume()
+    }
+
+    // MARK: - Helper Methods
+
+    private func didFetchWeatherData(data: Data?, response: URLResponse?, error: Error?, completion: WeatherDataCompletion) {
+        if let _ = error {
+            completion(nil, .FailedRequest)
+
+        } else if let data = data, let response = response as? HTTPURLResponse {
+            if response.statusCode == 200 {
+                processWeatherData(data: data, completion: completion)
+            } else {
+                completion(nil, .FailedRequest)
+            }
+
+        } else {
+            completion(nil, .Unknown)
+        }
+    }
+
+    private func processWeatherData(data: Data, completion: WeatherDataCompletion) {
+        if let JSON = try? JSONSerialization.jsonObject(with: data, options: []) as AnyObject {
+            completion(JSON, nil)
+        } else {
+            completion(nil, .InvalidResponse)
+        }
+    }
 
 }
