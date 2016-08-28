028e61b350c48dba7c61303b375ca19c89915c76
Bart Jacobs <developer@bartjacobs.com>
2016-08-28 15:06:03 +0200

    Create DataManager class

---
+++ Thunderstorm/Managers/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  Thunderstorm
+//
+//  Created by Bart Jacobs on 28/08/16.
+//  Copyright © 2016 Cocoacasts. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+
+}
